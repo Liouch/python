ab561ce9ecdba582db0d599cff727e186b1f943a
Liou Wang <liou.wang.it@gmail.com>
2020-04-25 18:23:08 +0200

    secret number recovered and added score.txt

--- Secret number.py
+++ Secret number.py
@@ -1,8 +1,26 @@
-secret = 1
+import random
 
-guess = input("Guess the secret number: ")
+secret = random.randint(1, 30)
+attempts = 0
 
-if int(guess) == secret:
-    print("Congratulations")
-else:
-    print("Wrong! Sorry")
+with open("score.txt", "r") as score_file:
+    best_score = int(score_file.read())
+    print("Top score (attempts): " + str(best_score))
+while True:
+    guess = int(input("Guess the secret number (between 1 and 30): "))
+    attempts += 1
+
+    if guess == secret:
+        if attempts < best_score:
+            with open("score.txt", "w") as score_file:
+                score_file.write(str(attempts))
+        print("You've guessed it - congratulations! It's number " + str(secret))
+        print("Attempts needed: " + str(attempts))
+        break
+    elif guess > secret:
+        print("Your guess is not correct... try something smaller")
+    elif guess < secret:
+        print("Your guess is not correct... try something bigger")
+    else:
+        print("algo fue mal")
+
