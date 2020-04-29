385695df3348355b3c3386799d9a56f51f1cef92
Liou Wang <liou.wang.it@gmail.com>
2020-04-29 15:10:54 +0200

    Secret number with top 3

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+import datetime
+
+box = {"height": 20, "width": 45, "length": 30}
+
+print(box["height"])
+print(box.get("height"))
+
+current_time = datetime.datetime.now()
+
+print(current_time)
--- Secret number top 3.py
+++ Secret number top 3.py
@@ -2,23 +2,35 @@ import random
 import json
 import datetime
 
+name = input("What's your name player?: ")
+
 secret = random.randint(1, 30)
 attempts = 0
 
 with open("score.txt", "r") as score_file:
     score_list = json.loads(score_file.read())
-    print("Top scores: " + str(score_list))
-for score_dict in score_list:
-    print(str(score_dict["attempts"]) + " attempts, date: " + score_dict.get("date") + " name: " + score_dict.get("name") + " Wrong guesses: " + score_dict.get("wrong_guesses"))
+    print("Top scores: " +str(score_list))
 
-name = input("What's your name player?: ")
+new_score_list = sorted(score_list, key=lambda k: k['attempts'])[:3]
+for score_dict in new_score_list:
+
+    for score_dict in new_score_list:
+        score_text = "Player {0} had {1} attempts on {2}. The secret number was {3}. The wrong guesses were: {4}".format(
+            score_dict.get("name"),
+            (score_dict.get("attempts")),
+            score_dict.get("date"),
+            score_dict.get("secret"),
+            score_dict.get("wrong_guesses"))
+
+    print(score_text)
+
 wrong_guesses = []
 while True:
     guess = int(input("Guess the secret number (between 1 and 30) " + name + ": "))
     attempts += 1
 
     if guess == secret:
-        score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), "name": name, "wrong_guesses" : wrong_guesses})
+        score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), "name": name, "wrong_guesses" : wrong_guesses, "secret_number": secret})
         with open("score.txt", "w") as score_file:
                 score_file.write(json.dumps(score_list))
         print(name + " You've guessed it - congratulations! It's number " + str(secret))
